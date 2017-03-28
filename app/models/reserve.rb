dfdfc344d42bb6e7fc4af74fd14093f3d5647316
andgursky <andgursky@gmail.com>
2017-03-28 04:26:10 +0300

    Fix a lot of bugs
    1. It validates when start 01:00am and and 05:00am
    2. It validates time overflow when reserve diapason is inside
    another reserve that starts at the time of the open restaurant
    at am and ends when it close in am (for example open 06:00am
    and close 05:00 am).
    3. It validates when update.

--- app/models/reserve.rb
+++ app/models/reserve.rb
@@ -2,43 +2,38 @@ class Reserve < ApplicationRecord
   belongs_to :user
   belongs_to :restaurant
 
-  #time_regexp = /(?<hours>\d{2}):(?<minuts>\d{2})/i
-  #validates :time_start, :time_end, format: { with: time_regexp,                                            message: "Time format 'hh:mm' only!" }
+  TIME_REGEXP = /(?<hours>\d{2}):(?<minutes>\d{2})/i
   NON_VALIDATABLE_ATTRS = ["id", "created_at", "updated_at"]
   VALIDATABLE_ATTRS = Reserve.attribute_names.reject do |attr|
    NON_VALIDATABLE_ATTRS.include?(attr)
   end
 
   validates_presence_of VALIDATABLE_ATTRS
-  after_validation :validate_time_overlap, :validate_reserve_valid_interval,
-    :validate_work_time_overflow, :validate_table_exist
-  # we need to validate inserting data to reserve
-  # - time start/end
+  #validates :time_start, :time_end, format:
+  #  { with: TIME_REGEXP, message: "Time format 'hh:mm' only!" }
+  after_validation :validate_time_overlap, :validate_table_exist,
+    :validate_reserve_valid_interval
 
   private
 
   def validate_time_overlap
     # check for reservation time overlaping
-  binding.pry
-    res = Reserve.where("user_id=? AND restaurant_id=? AND table_number=? AND"+
-                        " time_end>? AND time_start<?", self.user_id,
-                        self.restaurant_id, self.table_number, self.time_start,
-                        self.time_end)
-    unless res.empty?
-      errors.add(:reserve, 'Reservation time overlap error!')
+    unless start_grater_end?(self.time_start, self.time_end)
+      res = Reserve.where(str_sql_find_overlap)
+      unless res.empty?
+        errors.add(:reserve, 'Reservation time overlap error!')
+      end
     end
   end
 
   def validate_reserve_valid_interval
     # check for valid reservation interval (30 min)
-  binding.pry
     h_start, m_start = split_time_str(self.time_start)
     h_end, m_end     = split_time_str(self.time_end)
 
-
     h_end += 24 unless h_end >= h_start
 
-    if ((h_end - h_start) * 60 + m_end - m_start) % 30
+    if (((h_end - h_start) * 60 + m_end - m_start) % 30) != 0
       errors.add(:reserve, 'Reserve interval should be multiple by 30 (min)!')
     end
   end
@@ -46,7 +41,6 @@ class Reserve < ApplicationRecord
   def validate_work_time_overflow
     # start/end time of reserve shouldn't be less/grater then restaurant
     # work hours
-  binding.pry
     rest_start_work = Restaurant.where(id:self.restaurant_id).first.time_open
     rest_end_work   = Restaurant.where(id:self.restaurant_id).first.time_close
 
@@ -55,18 +49,24 @@ class Reserve < ApplicationRecord
     h_start, m_start = split_time_str(self.time_start)
     h_end, m_end     = split_time_str(self.time_end)
 
-    h_rest_end += 24 unless h_rest_end >= h_rest_start
-    h_end += 24 unless h_end >= h_start
+    h_start += 24 if h_rest_start > h_start && h_start < h_rest_end
+    h_rest_end += 24 if h_rest_end < h_rest_start
+    h_end += 24 if h_end < h_start
 
     if h_end > h_rest_end || h_start < h_rest_start
-      errors.add(:reserve, "Restaurant doesn't work in desired time interval?")
+      errors.add(:reserve, "Restaurant doesn't work in desired time interval!")
+      return true
+    else
+      self.time_start = "%.2i:%.2i" % [h_start, m_start]
+      self.time_end   = "%.2i:%.2i" % [h_end, m_end]
     end
+
+    false
   end
 
   def validate_table_exist
     # table number shouldn't be grater then tables count in this restaurant
     # and shouldn't be less or equal zero
-  binding.pry
     tables_count = Restaurant.where(id:self.restaurant_id).first.tables_count
 
     unless tables_count >= self.table_number && self.table_number > 0
@@ -78,4 +78,31 @@ class Reserve < ApplicationRecord
   def split_time_str(time)
     time.split(':').map(&:to_i)
   end
+
+  def start_grater_end?(time_start, time_end)
+    h_start, m_start = split_time_str(time_start)
+    h_end, m_end = split_time_str(time_end)
+
+    if validate_work_time_overflow
+      errors.add(:reserve, "Start time should be less then end time!")
+      return true
+    end
+
+    false
+  end
+
+  def str_sql_find_overlap
+    unless self.id
+      fmt = "user_id=%i AND restaurant_id=%i AND table_number=%i AND "+
+            "time_end>\'%s\' AND time_start<\'%s\'"
+      str = fmt % [self.user_id, self.restaurant_id, self.table_number,
+                   self.time_start, self.time_end]
+    else
+      fmt = "user_id=%i AND restaurant_id=%i AND table_number=%i AND "+
+            "time_end>\'%s\' AND time_start<\'%s\' AND id<>%i"
+      str = fmt % [self.user_id, self.restaurant_id, self.table_number,
+                   self.time_start, self.time_end, self.id]
+    end
+  end
+
 end
